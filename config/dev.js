00196bc5f42739654be4655b99404ac8baafe14d
Prabeen Kumar Soreng <prabeensoreng2119@gmail.com>
2019-09-14 18:50:15 +0530

    keys updated

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,8 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
+  googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
+  mongoURI:
+    "mongodb://prabeen:n2WR9WzdmRIwRyl6@cluster0-shard-00-00-5ikaf.mongodb.net:27017,cluster0-shard-00-01-5ikaf.mongodb.net:27017,cluster0-shard-00-02-5ikaf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
+  cookieKey: "123123123"
 };
